14ed3811e1f6e6f9622593a659247e9cd09272a6
Steve Miller <ben.el.elyon@gmail.com>
2019-03-25 22:18:50 -0400

    Working seeding function inside of index.js

--- server/database/index.js
+++ server/database/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const fake = require('faker');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/books');
 
 let bookSchema = mongoose.Schema({
@@ -8,7 +9,7 @@ let bookSchema = mongoose.Schema({
     unique: true
   },
   title: String,
-  author: [String],
+  author: String,
   description: String,
   ratings: {
     five: Number,
@@ -55,16 +56,64 @@ let bookSchema = mongoose.Schema({
 
 let Book = mongoose.model('Book', bookSchema);
 
-
-
-let seed = async () => {
+module.exports.seed = async () => {
+  // clean out current database, if any test records clogging up
   Book.deleteMany({}, async () => {
-    let bookExample = new Book();
+    var book;
+    for (var i = 0; i < 100; i++) {
+      book = new Book();
 
-    bookExample.title = "Harry Potter and the Goblet of Firey"
-    bookExample.id = 1
-    await bookExample.save();
+      // initiate a bunch of new book info
+      book.id = i;
+      book.title = fake.random.words(3);
+      book.author = fake.name.findName();
+      book.description = fake.lorem.paragraphs();
+      book.ratings = {
+        five: fake.random.number(),
+        four: fake.random.number(),
+        three: fake.random.number(),
+        two: fake.random.number(),
+        one: fake.random.number()
+      },
+      book.links = {
+        kindle: fake.internet.url(),
+        amazon: fake.internet.url(),
+        stores: {
+          audible: fake.internet.url(),
+          barnesAndNoble: fake.internet.url(),
+          walmart: fake.internet.url(),
+          apple: fake.internet.url(),
+          google: fake.internet.url(),
+          abebooks: fake.internet.url(),
+          bookDesository: fake.internet.url(),
+          indigo: fake.internet.url(),
+          alibris: fake.internet.url(),
+          betterWorldBooks: fake.internet.url(),
+          indieBound: fake.internet.url()
+        }
+      }
+      book.type = fake.random.word();
+      book.pages = fake.random.number({max: 3000});
+      book.publishdate = fake.date.past();
+      book.publisher = fake.company.companyName();
+      book.metadata = {
+        originalTitle: book.title,
+        isbn: fake.random.number(),
+        isbn13: fake.random.number(),
+        language: 'English'
+      }
+
+      if (i % 2 === 0) {
+        book.series = {
+          name: fake.random.words(2),
+          url: fake.internet.url()
+        }
+      }
+
+      await book.save();
+      console.log(`${i} saved as ${book.title}`)
+    }
   })
 }
 
-seed();
+module.exports.seed();
