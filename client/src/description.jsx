099273a959f9e816e0483bcfd19334025ff53bdb
Steve Miller <ben.el.elyon@gmail.com>
2019-04-15 14:50:17 -0400

    Styled description area, added descriptionText.jsx to flesh out more/less logic

--- client/src/description.jsx
+++ client/src/description.jsx
@@ -1,12 +1,69 @@
 import React from 'react';
+import styled from 'styled-components';
+import StarRatings from 'react-star-ratings';
+import DescriptionText from './descriptionText.jsx';
+
+const Title = styled.h1`
+  margin-bottom: 2px;
+  width: 100%;
+  font-weight: bold;
+  font-family: "Merriweather", "Georgia", serif;
+  color: #333333;
+  font-size: 24px;
+  line-height: 1.25;
+  margin-top: 10px;
+`
+
+const Author = styled.div`
+  color: #333;
+  font-size: 16px;
+  font-family: "Merriweather", "Georgia", serif;
+  margin-bottom: 8px;
+`
+const RatingsRow = styled.div`
+  display: flex;
+  font-family: "Lato", "Helvetica Neue", "Helvetica", sans-serif;
+  font-size: 12px;
+  margin-bottom: 10px;
+`
+
+const RatingsDot = styled.span`
+  color: lightgrey;
+`
+
+const RatingsText = styled.span`
+  color: #00635d;
+
+  :hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
 
 const Description = (props) => {
   return(
     <div>
-      <div className='summary'>
-        <h1>{props.title}</h1>
-        <h2>{props.author}</h2>
-        <p>{props.description}</p>
+      <div>
+        <Title>{props.title}</Title>
+        <Author>by {props.author}</Author>
+        <RatingsRow>
+          <StarRatings
+            rating={4}
+            starDimension="15px"
+            starSpacing="0px"
+            starRatedColor="rgb(250, 96, 74)"
+          />
+          <div>
+            4
+            <RatingsDot> · </RatingsDot>
+            <RatingsText>Rating details</RatingsText>
+            <RatingsDot> · </RatingsDot>
+            <RatingsText>2,073,822 ratings</RatingsText>
+            <RatingsDot> · </RatingsDot>
+            <RatingsText>33,328 reviews</RatingsText>
+          </div>
+        </RatingsRow>
+        <DescriptionText text={props.description} />
       </div>
       <div className='purchase'>
         <button type='button'>Kindle</button>
---
+++ client/src/descriptionText.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const DescriptionArea = styled.div`
+  font-family: "Merriweather", "Georgia", serif;
+  font-size: 14px;
+  line-height: 21px;
+`
+
+const ShowMore = styled.span`
+  color: #00635d;
+
+  :hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`
+
+class DescriptionText extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      full: false
+    }
+
+    this.toggleView = this.toggleView.bind(this);
+  }
+
+  toggleView() {
+    console.log('view toggled');
+    this.setState(state => {
+      return { full: !(state.full) }
+    })
+  }
+
+  render() {
+    let shortText = this.props.text + ''; // wtf
+    let toggle = ' (less)'
+    if (this.state.full === false) {
+      shortText = shortText.substring(0, 400);
+      toggle = ' ...more';
+    }
+    return(
+      <DescriptionArea>
+        {shortText}
+        <ShowMore onClick={this.toggleView}>{toggle}</ShowMore>
+      </DescriptionArea>
+
+    );
+  }
+}
+
+export default DescriptionText;
