06d0875fc0a4fa1199157b6b60445def8c568b44
Muhammad Jamaluddin <mohamed.eldeen.399@gmail.com>
2019-05-16 04:21:31 +0200

    Install morgan

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 // bundled with express by default
 // eslint-disable-next-line import/no-extraneous-dependencies
 const bodyParser = require('body-parser');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const morgan = require('morgan');
 const path = require('path');
 const books = require('./database/index.js');
 
@@ -10,10 +12,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/../client')));
 app.use('/:id', express.static(path.join(__dirname, '/../client')));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
